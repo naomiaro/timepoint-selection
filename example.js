41917bd60992d18e710317f185d35264dde9f809
Naomi Aro <naomiaro@gmail.com>
2016-05-19 18:47:22 +0100

    Updates

---
+++ example.js
@@ -0,0 +1,19 @@
+selection(document.getElementById("ts"));
+
+selection(document.getElementById("hhmmss"), {
+    'durationFormat': 'hh:mm:ss',
+    'max': 3600 * 24
+});
+
+selection(document.getElementById("hhmm"), {
+    'durationFormat': 'hh:mm'
+});
+
+selection(document.getElementById("hhmmmax"), {
+    'durationFormat': 'hh:mm',
+    'max': 3600 * 12
+});
+
+selection(document.getElementById("mmss"), {
+    'durationFormat': 'mm:ss'
+});
